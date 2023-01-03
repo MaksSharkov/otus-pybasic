7cabf790eb40e65d6309c75cb9c7853b4e9562e0
Максим Шарков <msharkov@nobilis.team>
2023-01-03 19:50:08 +0300

    Добавлены модели, изменен шаблон на вывод в табличку всех секретов, добавлен скрипт для заполнения БД демо данными.
    Signed-off-by: Максим Шарков <msharkov@nobilis.team>

--- homework_07/vault/storage/models.py
+++ homework_07/vault/storage/models.py
@@ -2,8 +2,28 @@ from django.db import models
 
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(verbose_name="name", max_length=50)
+
+    # На уроке говорили, что так не стоит делать, но без указания в админке сущности висят как Object (1) и т.д.
+    def __str__(self):
+        return f"({self.id}) {self.name}"
+
+
+class Type(models.Model):
+    name = models.CharField(verbose_name="name", max_length=50)
+
+    def __str__(self):
+        return f"({self.id}) {self.name}"
+
+
 class Secret(models.Model):
     name = models.CharField(verbose_name="name", max_length=50)
     value = models.TextField(verbose_name="value")
     desc = models.TextField(verbose_name="description", blank=True)
-    type = models.CharField(verbose_name="type", max_length=30)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"({self.id}) {self.project.name} | {self.name}"
